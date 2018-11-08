6e80ebb94f8e9674bf576a6c6d974988556a425b
Jhonatan da Silva Batista <talkto@jhonatansilva.com>
2018-11-08 02:10:17 -0200

    feat: add colors to design-system and set correct font size

---
+++ src/design-system/color.js
@@ -0,0 +1,8 @@
+export default {
+  white: '#fff',
+  black: '#444444',
+  'red-100': '#ff4e50',
+  'green-100': '#19d4ca',
+  'orange-100': '#ffa710',
+  'blue-100': '#667df3',
+};
--- src/design-system/index.js
+++ src/design-system/index.js
@@ -1,4 +1,5 @@
 import font from './font';
+import color from './color';
 
 export default {
   font: {
@@ -7,17 +8,26 @@ export default {
       secondary: font.family.roboto,
     },
     sizes: {
-      xs: '16px',
-      s: '20px',
-      base: '30px',
-      m: '36px',
-      l: '42px',
-      xl: '50px',
-      xxl: '58px',
+      xs: '10px',
+      s: '14px',
+      base: '16px',
+      m: '18px',
+      l: '20px',
+      xl: '22px',
+      xxl: '24px',
+      xxxl: '52px',
     },
     weight: {
       light: 300,
       regular: 400,
     },
+  },
+  color: {
+    primary: color['red-100'],
+    secondary: color.white,
+    success: color['green-100'],
+    warning: color['orange-100'],
+    black: color.black,
+    blue: color['blue-100'],
   },
 };
